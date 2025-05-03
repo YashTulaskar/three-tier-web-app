2f09e39d1f004fdc5123b05af435d266e0055bef
Yash Tulaskar <yashtulaskar8@gmail.com>
2025-05-03 22:15:12 +0530

    do it

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cluster-chcygc26e5qi.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Yash3036',
+    DB_DATABASE : 'webappdb'
+});
